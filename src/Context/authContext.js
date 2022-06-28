166d460a2df1562b110f60a286efd017bba447ee
Matthias Wieser <matthias.wieser.83@live.de>
2022-06-28 11:39:56 +0200

    added userId to the auth context

--- src/Context/authContext.js
+++ src/Context/authContext.js
@@ -6,6 +6,7 @@ export const authContext = createContext();
 const AuthState = ({ children }) => {
     const apiUrl = process.env.REACT_APP_API_URL;
     const [processing, setProcessing] = useState(true);
+    const [userId, setUserId] = useState(null);
     const token = localStorage.getItem('token');
     // const [token, setToken] = useState(localStorage.getItem('token'));
     const [verified, setVerified] = useState(null);
@@ -20,6 +21,7 @@ const AuthState = ({ children }) => {
                 }
             )
             .then(res => {
+                setUserId(res.data.userId);
                 setVerified(true);
                 setProcessing(false);
             })
@@ -32,7 +34,7 @@ const AuthState = ({ children }) => {
 
     if (!processing) {
         return (
-            <authContext.Provider value={{ setVerified, verified }}>
+            <authContext.Provider value={{ setVerified, verified, userId }}>
                 {children}
             </authContext.Provider>
         );
