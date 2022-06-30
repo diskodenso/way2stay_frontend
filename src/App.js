0ea5ed61493fe88d2a69f1744921012a69d050fc
Marcel-BK <MarcelBK@icloud.com>
2022-06-30 11:03:16 +0200

    fix Footer and add FlatsList.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import FlatsList from './Components/FlatsList';
 import Footer from './Components/Footer';
 import Landing from './Components/Landing';
 import Login from './Components/Login';
@@ -10,18 +11,18 @@ import UserDashboard from './Components/UserDashboard';
 
 const App = () => {
     return (
-        <div className="App font-text bg-[#f1f5ee]">
+        <div className="App font-text bg-[#f1f5ee] min-h-screen flex flex-col justify-between">
             <header className="App-header sticky top-0 z-50">
                 <NavBar />
             </header>
-            <main>
+            <main className='mb-auto'>
                 <Routes>
                 <Route path='/' element={<Landing />} />
                     <Route path='/signup' element={<Signup />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/logout' element={<Logout />} />
                     <Route path='/dashboard/' element={<UserDashboard />} />
-                    <Route path='/flats/' element={<h2>Flats</h2>} />
+                    <Route path='/flats/' element={<FlatsList />} />
                 </Routes>
             </main>
             <footer>
