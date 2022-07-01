263aec62f123040767f22e5ad7720945ecd1507d
Denso666 <Denisolf@mx.de>
2022-07-01 16:46:20 +0200

    add flatdetails component

--- src/Components/FlatDetail.js
+++ src/Components/FlatDetail.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { authContext } from "../Context/authContext.js";
+import SingleFlatMap from "./SingleFlatMap";
 
-const FlatDetail = () => {
+const FlatDetail = ({ flat }) => {
+  const { flatId } = useParams();
+  const { imgPlaceholder } = useContext(authContext);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [flat, setFlat] = useState(null);
+  const navigate = useNavigate();
+
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    if (flatId) {
+      axios
+        .get(`${apiUrl}/flats/${flatId}`)
+        .then((res) => {
+          setFlat(res.data.flat);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+          setError(err);
+          console.log(err);
+        });
+    } else {
+      setLoading(false);
+    }
+  }, [apiUrl, flatId, navigate]);
+
   return (
-    <div>FlatDetail</div>
-  )
-}
+    <>
+      <div className=' bg-[url("https://i.ibb.co/qJFwrYN/Landingpage-BG1.png")] w-full bg-no-repeat min-h-[73vh] mt-20'>
+        <div className="w-2/3 border rounded-lg p-5 shadow-lg border-[#b9b9b9] bg-white mx-auto">
+          <div className="items-center gap-5 mb-5">
+            <picture className="rounded-full w-[50px] h-[50px] bg-green"></picture>
+          </div>
+          <div className="flex justify-between items-center mt-8">
+            <h2>Details zur Wohnung:</h2>
+            <div className="flatDetailsContainer">
+              <h3>{flat.title}</h3>
+              <img src={imgPlaceholder} alt={"placeholder"} />
+              <p>{flat.description}</p>
+              <p>{flat.address.city}</p>
+              <SingleFlatMap />
+              <button
+                name="onClick"
+                type="onClick"
+                className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-white"
+              >
+                Buchen
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default FlatDetail
+export default FlatDetail;
