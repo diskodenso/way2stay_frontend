854c19182da5e1707dbf5b42861015f647ecae6c
Denso666 <Denisolf@mx.de>
2022-07-01 14:06:32 +0200

    finish about map

--- src/Components/AboutMap.js
+++ src/Components/AboutMap.js
@@ -1,39 +1,23 @@
+import React from "react";
 // import Map, Marker, Popup and title from leaflet
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
 
 const AboutMap = () => {
-  const [activePopup, setActivePopup] = useState(null);
-  return (
-    <>
-      <h1>You can find us here:</h1>
-      <MapContainer center={[54.525963, 15.255119]} zoom={12}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker
-          position={[52.457119, 13.54023]}
-          onClick={() => {
-            setActivePopup(true);
-          }}
-        />
-        {activePopup && (
-          <Popup
-            position={[52.457119, 13.54023]}
-            onClose={() => {
-              setActivePopup(null);
-            }}
-          >
-            <div>
-              <h2>Our beautiful Office</h2>
-              <p>Visit Us: Mo-Fr 10am - 6pm</p>
-            </div>
-          </Popup>
-        )}
-      </MapContainer>
-    </>
+  const position = [52.10696, 13.2726];
+
+  render(
+    <MapContainer center={position} zoom={16} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          This is our beautiful office. <br /> Come by Monday to Friday 10am to
+          6pm.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
-
 export default AboutMap;
