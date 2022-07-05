478b8b3908339266722cff0c85b2a9f19588d74d
Marcel-BK <MarcelBK@icloud.com>
2022-07-05 11:12:29 +0200

    add Flat Detail Reviews component

---
+++ src/Components/FlatDetailReviews.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const FlatDetailReviews = () => {
+  return (
+    <>
+      <div className="mb-5 p-5 rounded-lg shadow-lg border border-[#b3b3b3]">
+        <div className="flex justify-between mb-2">
+          <h4 className="font-heading text-lg">Name Nachname</h4>
+          <p className="text-yellow"># # # # #</p>
+        </div>
+        <p className="mr-20">
+          You go to their place. They come to yours. Just find a home you&apos;d
+          like to stay in, and see if the owner would like to stay in yours.
+          Classic Swaps can happen at the same time or at different times.
+        </p>
+        <div className="flex justify-end">04.07.2022</div>
+      </div>
+
+      <div className="mb-5 p-5 rounded-lg shadow-lg border border-[#b3b3b3]">
+        <div className="flex justify-between mb-2">
+          <h4 className="font-heading text-lg">Name Nachname</h4>
+          <p className="text-yellow"># # # # #</p>
+        </div>
+        <p className="mr-20">
+          You go to their place. They come to yours. Just find a home you&apos;d
+          like to stay in, and see if the owner would like to stay in yours.
+          Classic Swaps can happen at the same time or at different times. You
+          go to their place. They come to yours. Just find a home you&apos;d
+          like to stay in, and see if the owner would like to stay in yours.
+          Classic Swaps can happen at the same time or at different times.
+        </p>
+        <div className="flex justify-end">17.02.1687</div>
+      </div>
+    </>
+  );
+};
+
+export default FlatDetailReviews;
