0ea5ba3e4d518d8cb4c12929fcd1a9cbd54fdfd1
Marcel-BK <MarcelBK@icloud.com>
2022-07-05 13:43:15 +0200

    rework ReadMe and add star Icons to reviews

--- src/Components/CarouselContainer.js
+++ src/Components/CarouselContainer.js
@@ -9,16 +9,24 @@ const CarouselContainer = () => {
       <div className="flex gap-12 pr-10 pb-20">
         <div className="w-1/2 my-auto">
           <div className="flex w-3/5 mx-auto justify-between">
-            <h1 className="pb-10 font-heading text-3xl align">BeispielName</h1>
-            <p className="text-3xl text-yellow"># # # # #</p>
+            <h1 className="pb-10 font-heading text-3xl align">Matthias</h1>
+            <p className="text-2xl text-yellow">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+            </p>
           </div>
           <p className="w-2/3 mx-auto pb-10">
-            REZENSION Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-            duo dolores et ea rebum.
+            This was such a fantastic stay for my family, including 2 kids. The
+            home is gorgeous and well-equipped and the views are unparalleled!
+            We would love to come again.
           </p>
-          <Link to={'/flats/62bb072dd491a11505c2148f'} className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-[#f1f5ee]">
+          <Link
+            to={"/flats/62bb072dd491a11505c2148f"}
+            className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-[#f1f5ee]"
+          >
             Visit this home
           </Link>
         </div>
@@ -33,8 +41,14 @@ const CarouselContainer = () => {
       <div className="flex gap-12 pr-10 pb-20">
         <div className="w-1/2 my-auto">
           <div className="flex w-3/5 mx-auto justify-between">
-            <h1 className="pb-10 font-heading text-3xl align">BeispielName</h1>
-            <p className="text-3xl text-yellow"># # # # #</p>
+            <h1 className="pb-10 font-heading text-3xl align">Marcel</h1>
+            <p className="text-2xl text-yellow">
+              <i class="fa fa-star text-[#d9d9d9]"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+            </p>
           </div>
           <p className="w-2/3 mx-auto pb-10">
             REZENSION Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
@@ -42,7 +56,10 @@ const CarouselContainer = () => {
             aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
             duo dolores et ea rebum.
           </p>
-          <Link to={'/flats/:flatId'} className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-[#f1f5ee]">
+          <Link
+            to={"/flats/:flatId"}
+            className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-[#f1f5ee]"
+          >
             Visit this home
           </Link>
         </div>
@@ -57,8 +74,14 @@ const CarouselContainer = () => {
       <div className="flex gap-12 pr-10 pb-20">
         <div className="w-1/2 my-auto">
           <div className="flex w-3/5 mx-auto justify-between">
-            <h1 className="pb-10 font-heading text-3xl align">BeispielName</h1>
-            <p className="text-3xl text-yellow"># # # # #</p>
+            <h1 className="pb-10 font-heading text-3xl align">Disko Denso</h1>
+            <p className="text-2xl text-yellow">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+            </p>
           </div>
           <p className="w-2/3 mx-auto pb-10">
             REZENSION Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
@@ -66,7 +89,10 @@ const CarouselContainer = () => {
             aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
             duo dolores et ea rebum.
           </p>
-          <Link to={'/flats/:flatId'} className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-[#f1f5ee]">
+          <Link
+            to={"/flats/:flatId"}
+            className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-[#f1f5ee]"
+          >
             Visit this home
           </Link>
         </div>
