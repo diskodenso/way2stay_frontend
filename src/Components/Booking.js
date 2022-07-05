5557fd21c34bfa77bb2e15fa338aa74d645d0961
Denso666 <Denisolf@mx.de>
2022-07-05 16:34:27 +0200

    prepare booking for merge

--- src/Components/Booking.js
+++ src/Components/Booking.js
@@ -2,40 +2,58 @@ import axios from "axios";
 import { useState, useContext, useEffect } from "react";
 import { authContext } from "../Context/authContext";
 import Loader from "./Loader";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import FlatDetailCarousel from "./FlatDetailCarousel.js";
-
+import ToTopButton from "./ToTopButton";
+import SingleFlatMap from "./SingleFlatMap";
+import OwnBookingTimeSheets from "./OwnBookingTimeSheets";
+// import { format, parseISO } from "date-fns";
+// import timesheet und zeitdaten rausholen
 // erstelle booking
 // verified - wenn verified checken ob flatID existiert und wenn ja dann booking
 // create a booking - check ob flatId existiert
-const Booking = ({ flats }) => {
+const Booking = () => {
   const { flatId } = useParams();
-  const [booking, setBooking] = useState(null);
-  const { userId, user, verified } = useContext(authContext);
+  const [customerFlats, setFlatsCustomer] = useState(null);
+  const { user, verified } = useContext(authContext);
   const apiUrl = process.env.REACT_APP_API_URL;
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [isMatch, setIsMatch] = useState(false);
   const navigate = useNavigate(authContext);
-
+  const [customerTimeSheets, setCustomerTimeSheets] = useState(null);
   useEffect(() => {
     !verified && navigate(`/login`);
-    const bookingData = async () => {
-      if (verified) {
+    const customerBookingData = async () => {
+      if (verified && flatId) {
         try {
           await axios
-            .get(`${apiUrl}/bookings/flats/${flatId}`)
+            .get(`${apiUrl}/flats/${flatId}`)
             .then((res) => {
-              setLoading(false);
+              setFlatsCustomer(res.data.flat);
+              setLoading(true);
               setError(false);
-              setBooking(res.data);
-              console.log(booking);
             })
             .catch((err) => {
               setLoading(false);
               setError(err);
               console.log(err);
+            });
+          await axios
+            .get(`${apiUrl}/timesheets/flats/${flatId}`)
+            .then((res) => {
+              setCustomerTimeSheets(res.data.timeSheet);
+              console.log(res.data.timeSheet);
+              setLoading(false);
+              setError(null);
+            })
+            .catch((err) => {
+              setLoading(false);
+              setError(err);
+              console.log(err);
+              toast.error(
+                "Sorry, we couldn't get the timesheets you have requested"
+              );
             });
         } catch (err) {
           setLoading(false);
@@ -46,29 +64,31 @@ const Booking = ({ flats }) => {
         setLoading(false);
       }
     };
-    bookingData();
-  }, [apiUrl, navigate, verified]);
+    customerBookingData();
+  }, [apiUrl, navigate, verified, flatId]);
 
-  const handleClick = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("damn");
-    const { arrival, departure } = e.target;
-    await axios
-      .post(`${apiUrl}/bookings`, {
-        departure: departure.value,
-        arrival: arrival.value,
-      })
-      .then((res) => {
-        setLoading(false);
-        setBooking(res.data);
-        console.log(booking);
-        toast.success("Booking successfully requested");
-      })
-      .catch((err) => {
-        setLoading(false);
-        setError(err);
-        toast.error("Oh no, something went wrong with the booking");
-      });
+    // const { departure, arrival } = e.target;
+
+    // console.log(departure);
+    // await axios
+    //   .post(`${apiUrl}/bookings`, {
+    //     departure: departure.value,
+    //     arrival: arrival.value,
+    //   })
+    //   .then((res) => {
+    //     setLoading(false);
+    //     setBooking(res.data);
+    //     console.log(booking);
+    //     toast.success("Booking successfully requested");
+    //   })
+    //   .catch((err) => {
+    //     setLoading(false);
+    //     setError(err);
+    //     toast.error("Oh no, something went wrong with the booking");
+    //   });
   };
 
   if (loading) {
@@ -84,91 +104,76 @@ const Booking = ({ flats }) => {
         <div className="flex w-5/6 gap-5 m-auto md:flex-wrap lg:flex-nowrap justify-center items-start">
           <div className="sticky w-1/3 rounded-lg p-5 my-5 shadow-lg bg-white">
             <div className="flex justify-between mb-5">
-              <h3 className="font-heading text-2xl">
-                The home you want to share
-              </h3>
+              <h3 className="font-heading text-2xl">{customerFlats.title}</h3>
               <Link
-                to={"/flats/62bb072dd491a11505c2148f"}
+                to={`/flats/${flatId}`}
                 className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-white"
               >
                 View this flat
               </Link>
             </div>
-            <FlatDetailCarousel />
+            {/* <FlatDetailCarousel /> */}
             <div className="mt-5 w-5/6 h-60 mx-auto bg-lightgreen rounded-lg shadow-lg text-center">
-              Map
+              <SingleFlatMap />
             </div>
             <p className="ml-10 mt-5">
-              Straßenname 12 <br />
-              12345 Keinort <br />
-              Germany
+              {customerFlats.location.street}{" "}
+              {customerFlats.location.housenumber}
+              <br />
+              {customerFlats.location.postalcode} {customerFlats.location.city}
+              <br />
+              {customerFlats.country}
             </p>
           </div>
         </div>
 
-        <div className="flex w-5/6 gap-5 m-auto md:flex-wrap lg:flex-nowrap justify-center items-start">
-          <div className="sticky w-1/3 rounded-lg p-5 my-5 shadow-lg bg-white">
-            <h3 className="font-heading text-2xl mb-5">
-              The home you want to book
-            </h3>
-            <FlatDetailCarousel />
-            <div className="mt-5 w-5/6 h-60 mx-auto bg-lightgreen rounded-lg shadow-lg text-center">
-              Map
-            </div>
-            <div className="flex justify-between items-end mt-5">
-              <p className="ml-10">
-                Straßenname 12 <br />
-                12345 Keinort <br />
-                Germany
-              </p>
-              <Link
-                to={"/flats/62bb072dd491a11505c2148f"}
-                className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-white"
-              >
-                View this flat
-              </Link>
+        <OwnBookingTimeSheets />
+        <form onSubmit={handleSubmit}>
+          <div className="flex w-5/6 gap-5 m-auto md:flex-wrap lg:flex-nowrap justify-center items-start">
+            <div className="sticky w-1/3 rounded-lg p-5 my-5 shadow-lg bg-white">
+              <div className="flex justify-between items-center gap-5 mb-5">
+                <div className="flex gap-4">
+                  <i className="text-xl text-white text-center bg-green rounded-full w-8 h-8 fa fa-user"></i>
+                  <h3 className="font-heading text-2xl">{user.username}</h3>
+                </div>
+
+                <button
+                  type="submit"
+                  className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-white"
+                >
+                  Book now!
+                </button>
+              </div>
+
+              {/* <div className="flex">
+                <div className="w-1/2 m-5">
+                  <h3 className="pb-3 font-heading text-xl">Arrival</h3>
+                  <input
+                    name="arrival"
+                    className="p-2 rounded border-b-2 border-[#6b6b6b] focus:outline-none w-full mb-5"
+                    type="date"
+                    placeholder="YYYY-MM-DD"
+                    // defaultValue={
+                    //   booking && format(parseISO(booking.arrival), "yyyy-MM-dd")
+                    // }
+                  />
+                </div>
+                <div className="w-1/2 m-5">
+                  <h3 className="pb-3 font-heading text-xl">Departure</h3>
+                  <input
+                    name="departure"
+                    type="date"
+                    className="p-2 rounded border-b-2 border-[#6b6b6b] focus:outline-none w-full mb-5"
+                    placeholder="YYYY-MM-DD"
+                    // defaultValue={
+                    //   booking && format(parseISO(booking.arrival), "yyyy-MM-dd")
+                    // }
+                  />
+                </div>
+              </div> */}
             </div>
           </div>
-        </div>
-
-        <div className="flex w-5/6 gap-5 m-auto md:flex-wrap lg:flex-nowrap justify-center items-start">
-          <div className="sticky w-1/3 rounded-lg p-5 my-5 shadow-lg bg-white">
-            <div className="flex justify-between items-center gap-5 mb-5">
-              <div className="flex gap-4">
-                <i className="text-xl text-white text-center bg-green rounded-full w-8 h-8 fa fa-user"></i>
-                <h3 className="font-heading text-2xl">{user.username}</h3>
-              </div>
-
-              <Link
-                to={"/flats/62bb072dd491a11505c2148f"}
-                className="border-2 border-green rounded-md px-3 py-1 text-green font-bold hover:bg-green hover:text-white"
-              >
-                Book now!
-              </Link>
-            </div>
-
-            <div className="flex">
-              <div className="w-1/2 m-5">
-                <h3 className="pb-3 font-heading text-xl">Arrival</h3>
-                <input
-                  name="start"
-                  className="p-2 rounded border-b-2 border-[#6b6b6b] focus:outline-none w-full mb-5"
-                  type="date"
-                  placeholder="YYYY/MM/DD"
-                />
-              </div>
-              <div className="w-1/2 m-5">
-                <h3 className="pb-3 font-heading text-xl">Departure</h3>
-                <input
-                  name="end"
-                  type="date"
-                  className="p-2 rounded border-b-2 border-[#6b6b6b] focus:outline-none w-full mb-5"
-                  placeholder="YYYY/MM/DD"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        </form>
       </div>
     </>
   );
